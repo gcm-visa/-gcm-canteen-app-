a3c79391585482d926cacc497d4481c1db727614
gcm-visa <visa@gcm.edu.ph>
2025-07-29 21:00:17 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
-const CACHE_NAME = "gcm-canteen-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+const CACHE_NAME = 'canteen-cache-v1';
+const cacheFiles = [
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(cacheFiles)));
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+  }
 });
